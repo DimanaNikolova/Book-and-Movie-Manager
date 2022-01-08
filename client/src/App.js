2f141d91ab5a069bbac483c0948848bb8deb0031
DimanaNikolova <dimanannikolova@gmail.com>
2022-01-08 15:22:27 +0200

    Cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,7 @@ import './App.css'
 import HomePage from './components/HomePage/HomePage'
 import SignUpPage from './components/SignUpPage/SignUpPage'
 import SignInPage from './components/SignInPage/SignInPage'
-import APhotoADay from './components/APhotoADay/APhotoADay'
 import PageContainer from './components/PageContainer/PageContainer'
-import MarsWeather from './components/MarsWeather/MarsWeather'
 
 function App() {
     return (
@@ -17,8 +15,6 @@ function App() {
                     <Route exact path='/' component={HomePage} />
                     <Route path='/sign-up' component={SignUpPage} />
                     <Route path='/sign-in' component={SignInPage} />
-                    <Route path='/landing' component={APhotoADay} />
-                    <Route path='/mars-weather' component={MarsWeather} />
                 </PageContainer>
             </Switch>
         </div>
