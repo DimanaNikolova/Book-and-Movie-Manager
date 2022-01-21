b28314eafae563836e0accb018c842b65a5d4e0d
DimanaNikolova <dimanannikolova@gmail.com>
2022-01-21 13:27:50 +0200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,14 +3,12 @@ let express = require('express');
 const serverConfig = require('./config/server');
 
 let cors = require('cors');
-// const { authMiddleware } = require('./middleware/authMiddleware.js');
 const { initApplication } = require('./config/init.js');
 
 const routes = require('./routes');
 const app = express();
 
 app.use(cors());
-// app.use(authMiddleware);
 app.use(routes);
 
 initApplication()
