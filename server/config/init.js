15ebecd607a87927334ad69e7ad6e501d9c4314e
DimanaNikolova <dimanannikolova@gmail.com>
2022-01-21 13:35:23 +0200

    Cleanup

--- server/config/init.js
+++ server/config/init.js
@@ -1,9 +1,7 @@
 const { initDatabase } = require('./mongoose.js');
-const { initDb } = require('./db.js');
 
 const initApplication = async () => {
     await initDatabase();
-    await initDb();
 };
 
 module.exports = {
